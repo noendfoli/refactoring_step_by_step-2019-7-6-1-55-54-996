bc202000b3eca99b9912b2bd5520c76f2b2f543c
owen <649073762@qq.com>
2019-07-15 19:08:40 +0800

    finshed

--- src/main/java/com/tws/refactoring/Driver.java
+++ src/main/java/com/tws/refactoring/Driver.java
@@ -3,6 +3,14 @@ package com.tws.refactoring;
 public class Driver {
     private int age;
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public Driver(int age) {
         this.age = age;
     }
--- src/main/java/com/tws/refactoring/Police.java
+++ src/main/java/com/tws/refactoring/Police.java
@@ -2,7 +2,10 @@ package com.tws.refactoring;
 
 public class Police {
     public boolean checkDriver(Driver driver) {
-        if(driver.age >= 18) return true;
-        else return false;
+        if(driver.getAge() >= 18) {
+            return true;
+        } else{
+            return false;
+        }
     }
 }
